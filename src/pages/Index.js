1fd6b97c2aa98df66ee54e8de0bbc2105af91b74
Henry Wilson <20hgww@queensu.ca>
2024-06-29 15:30:13 -0400

    Modify index page

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -2,30 +2,43 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Main from '../layouts/Main';
+import ContactIcons from '../components/Contact/ContactIcons';
+
+const { PUBLIC_URL } = process.env;
 
 const Index = () => (
   <Main
     description={"Henry Wilson's personal website. New Applied Mathematics and Engineering Graduate from Queen's University"}
+    fullPage
   >
+    <section id="intro">
+      <Link to="/" className="logo">
+        <img src={`${PUBLIC_URL}/images/me.jpg`} alt="" />
+      </Link>
+      <header>
+        <h2>Henry Wilson</h2>
+        <p><a href="mailto:henrygwwilson@gmail.com">henrygwwilson@gmail.com</a></p>
+      </header>
+    </section>
+
     <article className="post" id="index">
       <header>
         <div className="title">
           <h2><Link to="/">About this site</Link></h2>
           <p>
-            A beautiful, responsive, statically-generated,
-            react application written with modern Javascript.
+            Hi, I&apos;m Henry. I am a graduate of <a href="https://www.queensu.ca/mathstat/sites/mastwww/files/uploaded_files/undergraduate/mthe/MathematicsEngineering-Brochure.pdf">Queen&apos;s University&apos;s Applied Mathematics and Engineering </a>
+            program. This website encapsulates my journey as a programmer.
+            Feel free to look around and reach out to me if you have any questions.
           </p>
+          {!window.location.pathname.includes('/about') ? <Link to="/about" className="button">Learn More</Link> : null}
         </div>
       </header>
-      <p> Welcome to my website. Please feel free to read more <Link to="/about">about me</Link>,
-        or you can check out my {' '}
-        <Link to="/resume">resume</Link>, {' '}
-        <Link to="/projects">projects</Link>, {' '}
-        view <Link to="/stats">site statistics</Link>, {' '}
-        or <Link to="/contact">contact</Link> me.
-      </p>
-      <p> Source available <a href="https://github.com/henrywils0n/personal-website">here</a>.</p>
     </article>
+
+    <section id="footer">
+      <ContactIcons />
+      <p className="copyright">&copy; Henry Wilson <Link to="/">henrywilson.com</Link>.</p>
+    </section>
   </Main>
 );
 
