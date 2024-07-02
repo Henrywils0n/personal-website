6ad62d47037ddb4e8f715a1df5b9c1306627ebf3
Henry Wilson <20hgww@queensu.ca>
2024-07-02 12:21:28 -0400

    add spotify API functionality

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Markdown from 'markdown-to-jsx';
-
 import Main from '../layouts/Main';
+import Personal from '../components/Stats/Personal';
+import { getAccessToken, getPlaylist } from '../utils/spotify';
 
 const About = () => {
   const [markdown, setMarkdown] = useState('');
+  const [playlist, setPlaylist] = useState(null);
 
   useEffect(() => {
     import('../data/about.md')
@@ -14,7 +16,15 @@ const About = () => {
           .then((r) => r.text())
           .then(setMarkdown);
       });
-  });
+
+    const fetchPlaylist = async () => {
+      const accessToken = await getAccessToken();
+      const playlistData = await getPlaylist(accessToken, '5oz1Md6FdhZLcpMeezKmyO');
+      setPlaylist(playlistData);
+    };
+
+    fetchPlaylist();
+  }, []);
 
   const count = markdown.split(/\s+/)
     .map((s) => s.replace(/\W/g, ''))
@@ -35,6 +45,24 @@ const About = () => {
         <Markdown>
           {markdown}
         </Markdown>
+        <article className="post" id="stats">
+          <header>
+            <div className="title">
+              <h2><Link to="/stats">Facts</Link></h2>
+            </div>
+          </header>
+          <Personal />
+        </article>
+        {playlist && (
+          <div className="playlist">
+            <h3>Playlist</h3>
+            <ul>
+              {playlist.tracks.items.map((track) => (
+                <li key={track.track.id}>{track.track.name}</li>
+              ))}
+            </ul>
+          </div>
+        )}
       </article>
     </Main>
   );
